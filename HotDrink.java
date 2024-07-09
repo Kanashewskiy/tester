d0008d5d4aa86fb0f67e84a2d12bf297ce518c91
«kanashewskiy» <kanashkirill5523@gmail.com>
2024-07-09 14:39:25 +0300

    commit all

---
+++ HotDrink.java
@@ -0,0 +1,36 @@
+public class HotDrink extends Product {
+
+    private int temperature;
+    private int volume;
+
+    public HotDrink(String name, int price, int temperature, int volume) {
+        super(name, price);
+        this.temperature = temperature;
+        this.volume = volume;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Автомат горячих напитков: { " +
+                "Наименование = " + getName() +
+                ", Температура = " + temperature +
+                ", Объём = " + volume +
+                '}';
+    }
+}
